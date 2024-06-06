c79dc720445c9b228773f07bfb592fc9e2367ebc
Peter Sedgewick <peter@primer.io>
2024-06-06 22:17:29 +0100

    fix: change to hashrouter for gh pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import Root from './Root'; // Ensure the path is correct
 import Genders from './Genders'; // Ensure the path is correct
 
